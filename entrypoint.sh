a20a8d53819e3359cde025af3375154eaa9642de
Lilin Lao <lll9p.china@gmail.com>
2021-10-13 14:43:55 +0800

    [init] initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ set -ex
 # 支持 host.docker.internal
 echo -e "`/sbin/ip route | awk '/default/ { print $3 }'`\thost.docker.internal" | tee -a /etc/hosts > /dev/null
 
+/usr/bin/frps -v
+
 case "$1" in
   frps)
     /usr/bin/frps -c /etc/frp/frps.ini
