a7d003d4de29121c0c2adfd7184f78e330657f06
lll9p <lll9p.china@gmail.com>
2023-11-02 10:24:37 +0800

    fix(entrypoint): update entrypoint.sh to use toml config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 
-echo "frp version is: `/usr/bin/frps -v`"
+echo "frp version is: $(/usr/bin/frps -v)"
 
 set -ex
 
 # 支持 host.docker.internal
-echo -e "`/sbin/ip route | awk '/default/ { print $3 }'`\thost.docker.internal" | tee -a /etc/hosts > /dev/null
+echo -e "$(/sbin/ip route | awk '/default/ { print $3 }')\thost.docker.internal" | tee -a /etc/hosts >/dev/null
 
 case "$1" in
-  frps)
-    /usr/bin/frps -c /etc/frp/frps.ini
-  ;;
-  frpc)
-    /usr/bin/frpc -c /etc/frp/frpc.ini
-  ;;
-  "")
-    /usr/bin/frps -c /etc/frp/frps.ini
-  ;;
+frps)
+	/usr/bin/frps -c /etc/frp/frps.toml
+	;;
+frpc)
+	/usr/bin/frpc -c /etc/frp/frpc.toml
+	;;
+"")
+	/usr/bin/frps -c /etc/frp/frps.toml
+	;;
 esac
